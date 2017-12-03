600777289844c5ed122b947760070e96cd5c9d8c
Holger Brandl <holgerbrandl@gmail.com>
2017-12-03 18:55:38 +0100

    finished part 1

--- src/main/kotlin/day3_spiral_memory.kt
+++ src/main/kotlin/day3_spiral_memory.kt
@@ -1,71 +1,59 @@
 import kotlin.coroutines.experimental.buildSequence
+import kotlin.math.abs
+
+
+data class DataElement(val data: Int, val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int)
+
 
 fun main(args: Array<String>) {
 
-
-    data class DataElement(val data: Int, val x: Int, val y: Int)
-    data class Point(val x: Int, val y: Int)
-
-
-    fun DataElement.next(grid: MutableList<DataElement>): DataElement {
+    fun DataElement.next(grid: List<DataElement>): DataElement {
         val context = listOf(
-            Point(x - 1, y),
-            Point(x, y - 1),
             Point(x + 1, y),
+            Point(x, y - 1),
+            Point(x - 1, y),
             Point(x, y + 1)
         )
 
         // make cyclic
         val circularized = context + context
 
-        val predecPos = with(grid.last()) { circularized.indexOf(Point(x, y)) }
-        val turnPos = circularized[predecPos + 1]
+        val lastEl = grid.last()
+        val predecPos = with(lastEl) { circularized.indexOf(Point(x, y)) }
+        val turn = circularized[predecPos + 1]
+        val forward = circularized[predecPos + 2]
 
-        return if (grid.firstOrNull { it.x == turnPos.x && it.y == turnPos.y } == null) {
-            return DataElement(data + 1, turnPos.x, turnPos.y)
+        // can we turn left, if not go straight in grid
+        return if (grid.firstOrNull { it.x == turn.x && it.y == turn.y } == null) {
+            return DataElement(data + 1, turn.x, turn.y)
         } else {
-            val forward = circularized[predecPos + 1]
             DataElement(data + 1, forward.x, forward.y)
         }
     }
 
 
-    // build spiral grid (https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.sequences/generate-sequence.html)
+    // build spiral grid (see https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.sequences/generate-sequence.html)
     val spiralSeq = buildSequence {
-        val grid = listOf(DataElement(0, 0, 0)).toMutableList()
+        val grid = listOf(DataElement(0, 0, 0), DataElement(1, 1, 0)).toMutableList()
         yield(grid.first())
 
-        var curEl = DataElement(0, 1, 0)
+        var curEl = grid.last()
 
         while (true) {
             yield(curEl)
-            curEl = curEl.next(grid)
+            if (grid.size.rem(10000) == 0) println("expanding grid to ${grid.size}")
+            curEl = curEl.next(grid.dropLast(1))
             grid.add(curEl)
         }
     }
 
     // puzzle input
-    val puzzleInput = 1024 // shoul be 31 steps
-    //    val puzzleInput = 265149 // actual
+    //    val puzzleInput = 1024 // shoul be 31 steps
+    val puzzleInput = 265149 // actual
     val take = spiralSeq.take(puzzleInput)
-    val spiralMemory = take.toList()
-    val numSteps = take.last().run { x + y }
+    val numSteps = take.last().run { abs(x) + abs(y) }
 
     println(numSteps)
-
-
-    //    fun DataElement.stepHome(): DataElement? {
-    //        if (data == 0) return null
-    //
-    //        val neighbors: List<DataElement> = with(listOf(0, 1, -1)) {
-    //            // build cartesian product to consider all neighbors
-    //            flatMap { i -> map { j -> i to j } }.map { (i, j) ->
-    //                spiralMemory.firstOrNull { it.x == i && it.y == j }
-    //            }.filterNotNull()
-    //        }
-    //
-    //        return neighbors.minBy { it.data }!!
-    //    }
-
 }
 

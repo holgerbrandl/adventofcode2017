60d67b672f5831b390bdf75ba8de63c0b95ece38
Holger Brandl <holgerbrandl@gmail.com>
2017-12-03 22:52:58 +0100

    fixed p2

--- src/main/kotlin/day3_spiral_memory.kt
+++ src/main/kotlin/day3_spiral_memory.kt
@@ -26,10 +26,12 @@ fun GridElement.next(grid: List<GridElement>): GridElement {
     // can we turn left? if not go straight in grid
     val newPos: Point = if (grid.byPosition(turn) == null) turn else forward
 
-    val neighborSum = (with(listOf(0, 1, -1)) {
+    val neigbors = with(listOf(0, 1, -1)) {
         flatMap { xOffset -> map { yOffset -> Point(newPos.x + xOffset, newPos.y + yOffset) } }
-    }.map { grid.byPosition(it) }.filterNotNull() + this).sumBy { it.sectorSum }
+    }.map { grid.byPosition(it) }.filterNotNull() + this
+    val neighborSum = neigbors.sumBy { it.sectorSum }
 
+    println("summing over ${neigbors.size} to $neighborSum")
 
     return GridElement(neighborSum, newPos.x, newPos.y)
 }
@@ -50,6 +52,7 @@ val spiralSeq = buildSequence {
 
         if (grid.size.rem(10000) == 0) println("expanding grid to ${grid.size}")
 
+        //        val dropInner = Math.min(0, 4 * Math.sqrt(grid.size.toDouble()).nextDown().toInt() -4)
         curEl = curEl.next(grid.dropLast(1))
         grid.add(curEl)
     }
@@ -57,20 +60,20 @@ val spiralSeq = buildSequence {
 
 fun main(args: Array<String>) {
 
-    //    val puzzleInput = 14 // shoul be 122 node sum
-    //    val puzzleInput = 1024 // shoul be 31 steps
+    //        val puzzleInput = 14 // shoul be 122 node sum
+    //        val puzzleInput = 1024 // shoul be 31 steps
+    //        val puzzleInput = 100
     val puzzleInput = 265149 // actual
-    val seq = spiralSeq.drop(puzzleInput - 1)
 
 
     // part 1
     // shortest manhattan path from puzzle to origin
-    val puzzleNode = seq.first()
+    val puzzleNode = spiralSeq.drop(puzzleInput - 1).first()
     println("num manhattan steps" + with(puzzleNode) { abs(x) + abs(y) })
 
     // part2
     // data for the node after the puzzle input
-    val afterPuzzleNode = seq.drop(1).first()
+    val afterPuzzleNode = spiralSeq.dropWhile { it.sectorSum < puzzleInput }.first()
     println("next puzzle sum: " + afterPuzzleNode.sectorSum)
 }
 

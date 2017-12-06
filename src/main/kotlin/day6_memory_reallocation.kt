7142df16b63c983fb68a20d30b6d134af67d1e64
Holger Brandl <holgerbrandl@gmail.com>
2017-12-06 11:49:54 +0100

    finished day6

--- src/main/kotlin/day6_memory_reallocation.kt
+++ src/main/kotlin/day6_memory_reallocation.kt
@@ -5,31 +5,34 @@
 
 fun main(args: Array<String>) {
     val input = "2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14".split(" ").map { it.toInt() }.toIntArray()
+    //        val input = listOf(0, 2, 7, 0).toIntArray()
 
-    //    val input = listOf(0, 2, 7, 0).toIntArray()
 
-
-    tailrec fun checkCycles(memBanks: IntArray, stateHistory: MutableSet<String> = mutableSetOf(memBanks.joinToString())): Int {
-        val maxBank = memBanks.indexOfFirst { it == memBanks.max() }
-
-        val numBlocks = memBanks[maxBank]
-        memBanks[maxBank] = 0
-
-        for (idx in (maxBank + 1) until (maxBank + 1 + numBlocks)) {
-            System.err.println("idx is ${idx % memBanks.size}")
-            memBanks[idx % memBanks.size] += 1
-        }
+    tailrec fun checkCycles(
+        memBanks: IntArray,
+        stateHistory: MutableList<String> = mutableListOf()
+    ): Pair<Int, Int> {
 
         val stateSnapshot = memBanks.joinToString()
         return if (stateHistory.contains(stateSnapshot)) {
-            stateHistory.size
+            val numInbetween = stateHistory.size - stateHistory.indexOf(stateSnapshot)
+            stateHistory.size to numInbetween
         } else {
-            System.err.println("adding $stateSnapshot")
-            stateHistory.add(stateSnapshot) //todo use for if
+            val maxBank = memBanks.indexOfFirst { it == memBanks.max() }
+
+            val numBlocks = memBanks[maxBank]
+            memBanks[maxBank] = 0
+
+            for (idx in (maxBank + 1) until (maxBank + 1 + numBlocks)) {
+                memBanks[idx % memBanks.size] += 1
+            }
+
+            stateHistory += stateSnapshot
             checkCycles(memBanks, stateHistory)
         }
     }
 
-    val numCycles = checkCycles(input)
-    println("numCycles unti same bank config: $numCycles")
+    val cycleHistory = checkCycles(input)
+    println("part 1: cyleHistory until same bank config: ${cycleHistory.first}")
+    println("part 2: cyleHistory in between same bank config: ${cycleHistory.second}")
 }

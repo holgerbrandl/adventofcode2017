3deb9d851c9a4cdbd2a599d2505d89a69797159c
Holger Brandl <holgerbrandl@gmail.com>
2017-12-15 09:02:17 +0100

    reworked p1

--- src/main/kotlin/day15_dueling_generators.kt
+++ src/main/kotlin/day15_dueling_generators.kt
@@ -3,31 +3,49 @@
  */
 
 fun main(args: Array<String>) {
+
+    //    // puzzle input
+    //    var startA = 883L
+    //    var startB = 879L
+
+    // test puzzle input
+    val startA = 65L
+    val startB = 8921L
+
+
     val genAFac = 16807
     val genBFac = 48271
 
-    val by = 2147483647
 
-    //    // puzzle input
-    var genA = 883L
-    var genB = 879L
+    fun Long.lower16() = Integer.toBinaryString(toInt())
+        .padStart(32, '0')
+        .substring(16, 32)
 
-    // test puzzle input
-    //    var genA = 65L
-    //    var genB = 8921L
+    // part1
+    fun generate(start: Long, factor: Int) = generateSequence(start) { (it * factor) % 2147483647 }.drop(1)
 
-
-    val numMatches = (0 until 40_000_000).sumBy {
-        genA = genA * genAFac % by
-        genB = genB * genBFac % by
-
-        if (genA.lower16() == genB.lower16()) 1 else 0
+    val take = generate(startA, genAFac).zip(generate(startB, genBFac)).take(40_000_000)
+    val numMatches = take.sumBy { (a, b) ->
+        if (a.lower16() == b.lower16()) 1 else 0
     }
 
-    println("lower 16 matches $numMatches")
+    println("part1 $numMatches")
+
+    // part2
+    //    val genDataA =
+
+    //    fun generateP2(start: Long, factor: Int, multipleOf: Int) = generateSequence(start) {
+    //        (it * factor % 2147483647).takeIf { it % multipleOf == 0L }
+    //    }.take(5_000_000).filterNotNull()
+    //
+    //
+    //    val zipped = generate(startA, genAFac, 4).zip(generate(startB, genBFac, 4)).toList()
+    //
+    //    val numP2Matches = zipped.sumBy { (genA, genB) ->
+    //        if (genA.lower16() == genB.lower16()) 1 else 0
+    //    }
+    //
+    //    println("part2 $numP2Matches")
 
 }
 
-fun Long.lower16() = Integer.toBinaryString(toInt())
-    .padStart(32, '0')
-    .substring(16, 32)
